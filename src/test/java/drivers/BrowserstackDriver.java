243b47b6e7b55215adb47bdd1e8c83d400ffab15
Zalina Kodz <kod3ik87@gmail.com>
2023-06-18 20:38:04 +0300

    changed pages

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -22,24 +22,15 @@ public class BrowserstackDriver implements WebDriverProvider {
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
         mutableCapabilities.merge(capabilities);
 
-        // Set your access credentials
         mutableCapabilities.setCapability("browserstack.user", config.getUser());
         mutableCapabilities.setCapability("browserstack.key", config.getKey());
-
-        // Set URL of the application under test
         mutableCapabilities.setCapability("app", config.getApp());
-
-        // Specify device and os_version for testing
         mutableCapabilities.setCapability("device", config.getDevice());
         mutableCapabilities.setCapability("os_version", config.getOsVersion());
-
-        // Set other BrowserStack capabilities
         mutableCapabilities.setCapability("project", config.getProjectName());
         mutableCapabilities.setCapability("build", config.getBuildName());
         mutableCapabilities.setCapability("name", config.getTestName());
 
-        // Initialise the remote Webdriver using BrowserStack remote URL
-        // and desired capabilities defined above
         try {
             return new RemoteWebDriver(new URL(config.getRemoteUrl()), mutableCapabilities);
         } catch (MalformedURLException e) {
